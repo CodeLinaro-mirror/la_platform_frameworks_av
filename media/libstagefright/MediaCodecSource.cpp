3591ca5ad6ed92e1399bf5a159b3f9025f1ce31c
Preetam Singh Ranawat <apranawat@codeaurora.org>
2016-04-14 12:10:06 +0800

    audio: add support for error handling of dsp SSR
    audio: Support for compress offload recovery
    Change-Id: I08a5ea333732b76bb9df110d905daae074b7864b
    
    Audio: post error to client during SSR
    Change-Id: Idf10939f1eb0c719a692e4f53f6e3040633fab40
    
    nuplayer: update anchor time for offload usecase
    Change-Id: Ib6e0f251207c87ad1525041d07187cef91f4dda5
    
    NuPlayer: fix possible deadlock due to recursive mutex access
    Change-Id: Iefdd061d745f548a207dd96f15c323fa7900d40d
    
    Change-Id: I6404f6425d7b17260ade83f2681eacf31db488c5

--- media/libstagefright/MediaCodecSource.cpp
+++ media/libstagefright/MediaCodecSource.cpp
@@ -669,6 +669,9 @@ void MediaCodecSource::signalEOS(status_t err) {
             output->mBufferQueue.clear();
             output->mEncoderReachedEOS = true;
             output->mErrorCode = err;
+            if (err != ERROR_END_OF_STREAM) {
+                output->mErrorCode = ERROR_IO;
+            }
             if (!(mFlags & FLAG_USE_SURFACE_INPUT)) {
                 mStopping = true;
                 mPuller->stop();
@@ -766,7 +769,7 @@ status_t MediaCodecSource::feedEncoderInputBuffers() {
             if (err != OK || inbuf == NULL || inbuf->data() == NULL
                     || mbuf->data() == NULL || mbuf->size() == 0) {
                 mbuf->release();
-                signalEOS();
+                signalEOS(err);
                 break;
             }
 
@@ -958,7 +961,7 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
             status_t err = mEncoder->getOutputBuffer(index, &outbuf);
             if (err != OK || outbuf == NULL || outbuf->data() == NULL
                 || outbuf->size() == 0) {
-                signalEOS();
+                signalEOS(err);
                 break;
             }
 
@@ -1037,7 +1040,7 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
                 mStopping = true;
                 mPuller->stop();
             }
-            signalEOS();
+            signalEOS(err);
        }
        break;
     }
