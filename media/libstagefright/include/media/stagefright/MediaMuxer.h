10fec22294ba519d5bb13b9eb59c9f2b8fda6412
Shubham Talekar <stalek@codeaurora.org>
2021-02-17 13:26:52 +0530

    MPEG4Writer:64bit offset + fallocate
    Use 64bit offset in mpeg4writer. Allows us to compose/mux files more
    than 4GB in size.  Notify MediaMuxer and MediaRecorder users about any failure during
    write() and seek(), with best effort attempt to save moov box.
    SDK and NDK MediaMuxer.WriteSampleData() would throw IllegalStateException and return
    INVALID_OPERATION as before, but in the very next call to this function after failure.
    Earlier, writeSampleData wasn't aware of any write/seek failure.
    SDK MediaMuxer.Stop() would throw relevant exception with error message.
    NDK stop() would return relevant errors.
    fallocate() is used to pre-allocate space for MOOV atom dynamically during
    run time. If required space couldn't be allocated, then recording is
    stopped and moov atom would be written to the file successfully.
    Added logic to stop and release looper.
    Bug: 135685864
    Bug: 142060459
    Bug: 142915394
    Bug: 145235586
    Test:  1) atest CtsMediaTestCases -- \
        --module-arg CtsMediaTestCases:size:small
           2) Composed files of size around 32GB using MediaMuxer and it is playing fine on the phone
                and linux workstation.
           3) Filled up most of the flash memory in phone and tried to compose files using MediaMuxer
                and MediaRecorder separately. Writing was stopped  when ::write failed due to out of space,
        and all threads exited gracefully in both cases. Expected errors were reported in Java layer.
                (pre-allocation was disabled in this step).
           4) The same experiment as in step 3 with pre-allocation enabled, recording was stopped
        when there was not enough space and moov atom was written to the output file. File played
        well on phone and linux workstation.
           5) atest android.media.cts.MediaMuxerTestMPEG4Writer:64bit offset + fallocate
    
    Change-Id: Ib57f585e80df6e92952c67034ea20bcb02a9bfbb

--- media/libstagefright/include/media/stagefright/MediaMuxer.h
+++ media/libstagefright/include/media/stagefright/MediaMuxer.h
@@ -117,6 +117,8 @@ public:
     status_t writeSampleData(const sp<ABuffer> &buffer, size_t trackIndex,
                              int64_t timeUs, uint32_t flags) ;
 
+    void notify(int msg, int ext1, int ext2);
+
 private:
     const OutputFormat mFormat;
     sp<MediaWriter> mWriter;
@@ -129,9 +131,11 @@ private:
         UNINITIALIZED,
         INITIALIZED,
         STARTED,
-        STOPPED
+        STOPPED,
+        ERROR
     };
     State mState;
+    status_t mError;
 
     DISALLOW_EVIL_CONSTRUCTORS(MediaMuxer);
 };
