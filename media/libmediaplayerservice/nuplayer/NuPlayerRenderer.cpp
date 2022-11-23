0d782477d4a065c6f04f765a04364a8a3f2d70c5
Shubham Jain <quic_sjai@quicinc.com>
2022-11-23 19:06:11 +0530

    frameworks:av:nuplayer: Ensure using video TS as anchor when really has no audio
    configPlayback() may be called before onQueueBuffer()(where we can
    actually know if stream contains audio and video), add more clear
    restrictions to ensure video only update anchor time in real video
    only case when playback rate changing.
    
    Change-Id: I585754358001bf31d71702f103ddabcfd2647cee

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -237,7 +237,7 @@ status_t NuPlayer::Renderer::onConfigPlayback(const AudioPlaybackRate &rate /* s
         }
     }
 
-    if (!mHasAudio) {
+    if (!mHasAudio && mHasVideo) {
        mNeedVideoClearAnchor = true;
     }
 
@@ -1347,7 +1347,7 @@ void NuPlayer::Renderer::postDrainVideoQueue() {
 
     {
         Mutex::Autolock autoLock(mLock);
-        if (mNeedVideoClearAnchor) {
+        if (mNeedVideoClearAnchor && !mHasAudio) {
             mNeedVideoClearAnchor = false;
             clearAnchorTime();
         }
