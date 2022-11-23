98ec72f82b877383415d92c1a832deaf6ec8c617
Shubham Jain <quic_sjai@quicinc.com>
2022-11-23 18:57:24 +0530

    frameworks:av:nuplayer: Video should clear and update anchor at the same time
    Change-Id: I023772e582c8a26e5b8e8e91e896631f425e03cd
    (cherry picked from commit 68fffc2131561b429a5a41109ca4d09fed36f644)
    (cherry picked from commit 22c96c7112c8dda7a8ed33b95878aa9049047f19)
    (cherry picked from commit 261600248a99355f6349d415c80d6041af138a9f)
    (cherry picked from commit db5e3955e4c14a9762e85f53f424766779d77a83)

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -240,6 +240,11 @@ status_t NuPlayer::Renderer::onConfigPlayback(const AudioPlaybackRate &rate /* s
             return err;
         }
     }
+
+    if (!mHasAudio) {
+       mNeedVideoClearAnchor = true;
+    }
+
     mPlaybackSettings = rate;
     mPlaybackRate = rate.mSpeed;
     mMediaClock->setPlaybackRate(mPlaybackRate);
@@ -1360,6 +1365,10 @@ void NuPlayer::Renderer::postDrainVideoQueue() {
 
     {
         Mutex::Autolock autoLock(mLock);
+        if (mNeedVideoClearAnchor) {
+            mNeedVideoClearAnchor = false;
+            clearAnchorTime();
+        }
         if (mAnchorTimeMediaUs < 0) {
             mMediaClock->updateAnchor(mediaTimeUs, nowUs, mediaTimeUs);
             mAnchorTimeMediaUs = mediaTimeUs;
