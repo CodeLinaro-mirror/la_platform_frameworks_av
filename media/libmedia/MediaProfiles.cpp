9aaf61697f60fbee0e0eb64d491fbe23b183a5fc
sasevs <quic_sasevs@quicinc.com>
2023-01-09 19:54:12 +0530

    StagefrightRecorder: Reset AudioSourceNode during LPCM recording.
    AudioSource is created as part of ExtendedAudioSource for LPCM
    recording. AudioSourceNode is not set to the right AudioSource
    in this case. Amplitude is not shown in SoundRecorder application
    due to this reason
    Fix is to reset AudioSourceNode to the instance that is returned
    by setPCMRecording
    CRs-Fixed: 2068175
    Change-Id: I13e6a78b8bdb1e5a23c398508100617e6fc8201a
    
    StagefrightRecorder: add null check for recording source
    Check that the recording source returned is not null before
    trying to add it to the writer. Writer will not check
    that this source is not null before dereferencing it.
    CRs-Fixed: 1085154
    Change-Id: I8dda57f39bbf5e289facc5ce261d53d988630620
    
    libmedia: correct latency computing of AudioRecord
    - Originally, audio record latency is calculated by frame
      buffers allocated for this track, while the actual
      latency is determined by audio hal.
    - Compute the track latency by frame count returned
      from audio hal when in TRANSFER_CALLBACK mode
    CRs-fixed: 1062825
    Change-Id: I26e5e47e8cc3720895b962f7aab8a595a54b7c83
    
    libmedia : Fix for AudioRecord CTS test case
      -if sample rate is not specificed during construction of audio record
       it updates sampling rate with default value based on the source.
       but divide by zero execption is happeing since
       it uses same unspeficied sampling rate to calculate latency.
      -Use updated default sample rate to calcuate latency vaulue.
    CRs-Fixed: 1090804
    Change-Id: Icb1e717d869de7bcd4b52194f3ee4e8b0b318da4
    
    StagefrightRecorder: Reset AudioSourceNode during LPCM recording.
    AudioSource is created as part of ExtendedAudioSource for LPCM
    recording. AudioSourceNode is not set to the right AudioSource
    in this case. Amplitude is not shown in SoundRecorder application
    due to this reason
    Fix is to reset AudioSourceNode to the instance that is returned
    by setPCMRecording
    CRs-Fixed: 2068175
    Change-Id: I13e6a78b8bdb1e5a23c398508100617e6fc8201a
    
    audio: calculate the max buffer size runtime
    Currently kMaxBufferSize is predefined as 30720,
    but this is valid only for camcorder use-case and
    not for other use-case, but as this is predefined
    calculations for other than camera use-case go
    wrong, such as frameCount and record latency, which
    leads to issues such as data drop at the begining
    due to high record latency.
    So instead predefining the MaxBufferSize calculate
    it only for camcorder use-case and use standard AOSP
    value otherwise.
    Change-Id: Ie7d43ddfdcb2b13a8a5eaa12cc8a233ac0d9470d
    
    Change-Id: I9f489564e35918d91e3034fc565320d0cf2d97be
    (cherry picked from commit 9af949191132938ffa812fb8e05c28619b84c71f)
    (cherry picked from commit de6cb8bc734dde83a7954cbb8c7416cb45c79b05)

--- media/libmedia/MediaProfiles.cpp
+++ media/libmedia/MediaProfiles.cpp
@@ -117,7 +117,8 @@ const MediaProfiles::NameToTagMap MediaProfiles::sAudioEncoderNameMap[] = {
     {"aac",    AUDIO_ENCODER_AAC},
     {"heaac",  AUDIO_ENCODER_HE_AAC},
     {"aaceld", AUDIO_ENCODER_AAC_ELD},
-    {"opus",   AUDIO_ENCODER_OPUS}
+    {"opus",   AUDIO_ENCODER_OPUS},
+    {"lpcm",  AUDIO_ENCODER_LPCM}
 };
 
 const MediaProfiles::NameToTagMap MediaProfiles::sFileFormatMap[] = {
@@ -798,8 +799,10 @@ MediaProfiles::startElementHandler(void *userData, const char *name, const char 
 }
 
 static bool isCamcorderProfile(camcorder_quality quality) {
-    return quality >= CAMCORDER_QUALITY_LIST_START &&
-           quality <= CAMCORDER_QUALITY_LIST_END;
+    return (quality >= CAMCORDER_QUALITY_LIST_START &&
+           quality <= CAMCORDER_QUALITY_LIST_END) ||
+           (quality >= CAMCORDER_QUALITY_VENDOR_LIST_START &&
+           quality <= CAMCORDER_QUALITY_VENDOR_LIST_END);
 }
 
 static bool isTimelapseProfile(camcorder_quality quality) {
@@ -1168,6 +1171,7 @@ MediaProfiles::createDefaultCamcorderProfiles(MediaProfiles *profiles)
 MediaProfiles::createDefaultAudioEncoders(MediaProfiles *profiles)
 {
     profiles->mAudioEncoders.add(createDefaultAmrNBEncoderCap());
+    profiles->mAudioEncoders.add(createDefaultLpcmEncoderCap());
 }
 
 /*static*/ void
@@ -1200,6 +1204,14 @@ MediaProfiles::createDefaultAmrNBEncoderCap()
 {
     return new MediaProfiles::AudioEncoderCap(
         AUDIO_ENCODER_AMR_NB, 5525, 12200, 8000, 8000, 1, 1);
+}
+
+
+/*static*/ MediaProfiles::AudioEncoderCap*
+MediaProfiles::createDefaultLpcmEncoderCap()
+{
+    return new MediaProfiles::AudioEncoderCap(
+        AUDIO_ENCODER_LPCM, 768000, 4608000, 8000, 48000, 1, 6);
 }
 
 /*static*/ void
