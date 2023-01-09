83e7bf737f7752131dcacee98e7e155bbb7835f4
sasevs <quic_sasevs@quicinc.com>
2023-01-09 20:13:06 +0530

    audiopolicy: Handle startOutput on output command thread
    Handle startOutput on AudioPolicyService output command
    thread to serialize with concurrent releaseOutput/
    stopOutput call.
    CRs-Fixed: 944129
    Original Change-Id: Ie333c736750c7dfb31d3036d79dfff13cd0486fc
    
    Change-Id: I5916ad425b227feee621359928f7a7a0f54ba010

--- services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
@@ -532,6 +532,16 @@ Status AudioPolicyService::startOutput(int32_t portIdAidl)
         return binderStatusFromStatusT(NO_INIT);
     }
     ALOGV("startOutput()");
+    mOutputCommandThread->startOutputCommand(portId);
+    return Status::ok();
+}
+
+status_t AudioPolicyService::doStartOutput(audio_port_handle_t portId)
+{
+    if (mAudioPolicyManager == NULL) {
+        return NO_INIT;
+    }
+    ALOGV("doStartOutput()");
     sp<AudioPlaybackClient> client;
     sp<AudioPolicyEffects> audioPolicyEffects;
 
@@ -556,7 +566,7 @@ Status AudioPolicyService::startOutput(int32_t portIdAidl)
         client->active = true;
         onUpdateActiveSpatializerTracks_l();
     }
-    return binderStatusFromStatusT(status);
+    return status;
 }
 
 Status AudioPolicyService::stopOutput(int32_t portIdAidl)
