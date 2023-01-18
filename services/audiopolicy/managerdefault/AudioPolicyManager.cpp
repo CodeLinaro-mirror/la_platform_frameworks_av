7a6e1b0810dc9efec30fd65fb5522fe83b2a2d64
sasevs <quic_sasevs@quicinc.com>
2023-01-18 15:46:18 +0530

    av: Fix for build issues while upgrading to T
     This will fix the compilation issues, which
     arised while upgrading to Android Version T.
    
    Change-Id: I87f36858189f3fedfe7a04ccdca97e88b83a6d3b
    (cherry picked from commit c3c4e1c317cdecd78d3b0570ebf2abd589ef2e30)
    (cherry picked from commit 7e0b9794b125ebe0a08c4ff529dcc7f788ee7285)

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -1705,7 +1705,7 @@ status_t AudioPolicyManager::getOutputForAttr(const audio_attributes_t *attr,
 
     status_t status = getOutputForAttrInt(&resultAttr, output, session, attr, stream, uid,
             &directConfig, flags, selectedDeviceId, &isRequestedDeviceForExclusiveUse,
-            secondaryOutputs != nullptr ? &secondaryMixes : nullptr, outputType);
+            secondaryOutputs != nullptr ? &secondaryMixes : nullptr, outputType, isSpatialized);
 //            isBitPerfect);
     if (status != NO_ERROR) {
         return status;
@@ -5308,6 +5308,7 @@ bool AudioPolicyManager::isOffloadPossible(const audio_offload_info_t &offloadIn
                   __func__, OFFLOAD_DEFAULT_MIN_DURATION_SECS);
             return false;
         }
+    }
     }
 
     // Do not allow offloading if one non offloadable effect is enabled. This prevents from
