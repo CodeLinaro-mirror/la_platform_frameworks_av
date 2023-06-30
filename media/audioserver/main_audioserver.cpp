2aecbdbb69e92b96d211ba5c3c1ffde2e06630fc
Sahitya Puligadda <quic_spuligad@quicinc.com>
2023-06-30 17:04:45 +0530

    Remove instantiateVRAudio Server for 1G memory optimization
    Change-Id: I68a2be40588124efcb10ee37fddaac556cfec956
    (cherry picked from commit e3a57a804302e428d5e954cc8520b19616ebaf65)

--- media/audioserver/main_audioserver.cpp
+++ media/audioserver/main_audioserver.cpp
@@ -167,8 +167,7 @@ int main(int argc __unused, char **argv)
         AudioFlinger::instantiate();
         ALOGI("ServiceManager: AudioFlinger instantiate done %p", sm.get());
         AudioPolicyService::instantiate();
-        ALOGI("ServiceManager: AudioPolicyService instantiate done %p", sm.get());
-        instantiateVRAudioServer();
+        //instantiateVRAudioServer();
 
         // AAudioService should only be used in OC-MR1 and later.
         // And only enable the AAudioService if the system MMAP policy explicitly allows it.
