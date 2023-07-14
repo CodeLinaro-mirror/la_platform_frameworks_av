64bf5bf7067c6b6f4376a41e87f5fa619eaaad7c
Sahitya Puligadda <quic_spuligad@quicinc.com>
2023-07-14 11:36:06 +0530

    Fix frameworks audioserver.cpp
    Change-Id: I19de119d434a25f3f793499a1e156bd73908aab2

--- media/audioserver/main_audioserver.cpp
+++ media/audioserver/main_audioserver.cpp
@@ -166,7 +166,7 @@ int main(int argc __unused, char **argv)
         AudioPolicyService::instantiate();
         ALOGI("ServiceManager: AudioPolicyService instantiate done %p", sm.get());
 
-        if (enable1GLowMem){
+        if (!enable1GLowMem){
         instantiateVRAudioServer();
         ALOGI("ServiceManager: VRAudioServer instantiate done %p", sm.get());
         }
