14581f37adee8bf3769929909f253894b60465b5
Nagarjuna Paladugu <quic_npaladug@quicinc.com>
2024-04-17 15:07:13 +0530

    audiopolicy: Add legacy ll support for voice comm record usecase
    Add legacy ll support for voice comm test input use case
    
    Change-Id: I2a48b8fb12e84d39a124a377fa87c4fe31bcfa9d
    (cherry picked from commit fac13c7178364ccd10a897c28cd25e1613343f73)
    (cherry picked from commit 6ba17eec410be923d61746e8b26f5b5d80ad5a3f)

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -3382,6 +3382,9 @@ audio_io_handle_t AudioPolicyManager::getInputForDevice(const sp<DeviceDescripto
         if ((flags & AUDIO_INPUT_FLAG_MMAP_NOIRQ) != 0) {
             flags = (audio_input_flags_t)AUDIO_INPUT_FLAG_MMAP_NOIRQ;
         }
+        else if ((flags & AUDIO_INPUT_FLAG_FAST) != 0) {
+            flags = (audio_input_flags_t)AUDIO_INPUT_FLAG_FAST;
+        }
         else {
             flags = (audio_input_flags_t)(flags | AUDIO_INPUT_FLAG_VOIP_TX);
         }
