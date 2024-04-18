57c6b6b996858a65ec1b8737a4312af589f58738
Rahul Ratneshwar Mandal <quic_rratnesh@quicinc.com>
2024-04-18 17:29:23 +0530

    CCodec: Align the client input buffer size to 256 for input CSD buffers
    Few xTS testcses are failing because the size of input CSD buffers are not aligned to 256 and video driver is returning error.
    So aligning the client input buffers size to 256 and passing it to the buffer channel
    
    Change-Id: If8d21e1a59a1580c97c1fb6e594c212b161c8ef3
    (cherry picked from commit 8cef1a4a214854bf4782d7954fe2228dfd312629)

--- media/codec2/sfplugin/CCodec.cpp
+++ media/codec2/sfplugin/CCodec.cpp
@@ -67,6 +67,7 @@
 #include "CCodecConfig.h"
 #include "Codec2Mapper.h"
 #include "InputSurfaceWrapper.h"
+#define ALIGN(_sz, _align) (((_sz) + ((_align) - 1)) & ~(_align))
 
 extern "C" android::PersistentSurface *CreateInputSurface();
 
@@ -1651,6 +1652,9 @@ void CCodec::configure(const sp<AMessage> &msg) {
                 if (csd && csd->size() > maxInputSize.value) {
                     maxInputSize.value = csd->size();
                 }
+            }
+            if (csd) {
+                clientInputSize = ALIGN(clientInputSize, 256);
             }
         }
 
