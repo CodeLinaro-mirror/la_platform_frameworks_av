437e9679be11c3cd68ec8361753212eec4382922
Shalini Manjunatha <quic_c_shalma@quicinc.com>
2024-07-04 11:58:00 +0530

    av: update minimum capture buf size
    Update the minimum capture buf size in milliseconds for flag
    AUDIO_INPUT_FLAG_NONE
    
    Change-Id: I9e0e6ed218882a9cb996ecf3d1c775b42c8addb2
    (cherry picked from commit 77994aee76fad4dd8db8cd71fc5aa2341b23cf55)
    (cherry picked from commit 96eb5e6287bfcfa8b582e1fd49100c23aaa6c54f)

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -14,6 +14,11 @@
 ** See the License for the specific language governing permissions and
 ** limitations under the License.
 */
+/* Changes from Qualcomm Innovation Center are provided under the following license:
+**
+** Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+** SPDX-License-Identifier: BSD-3-Clause-Clear.
+*/
 
 
 #define LOG_TAG "AudioFlinger"
@@ -188,7 +193,9 @@ static const uint32_t kMaxNormalSinkBufferSizeMs = 24;
 
 // minimum capture buffer size in milliseconds to _not_ need a fast capture thread
 // FIXME This should be based on experimentally observed scheduling jitter
-static const uint32_t kMinNormalCaptureBufferSizeMs = 30;
+static const uint32_t kMinNormalCaptureBufferSizeMs = 32;
+// For audio_input_flags_t flag None
+static const uint32_t kMinNormalCaptureBufferSizeMs_NonLL = 12;
 
 // Offloaded output thread standby delay: allows track transition without going to standby
 static const nsecs_t kOffloadStandbyDelayNs = seconds(1);
@@ -8368,7 +8375,8 @@ RecordThread::RecordThread(const sp<IAfThreadCallback>& afThreadCallback,
     case FastCapture_Static:
         initFastCapture = !mIsMsdDevice // Disable fast capture for MSD BUS devices.
                 && audio_is_linear_pcm(mFormat)
-                && (mFrameCount * 1000) / mSampleRate <= kMinNormalCaptureBufferSizeMs;
+                && (mFrameCount * 1000) / mSampleRate < (mInput->flags == AUDIO_INPUT_FLAG_NONE ?
+                kMinNormalCaptureBufferSizeMs_NonLL : kMinNormalCaptureBufferSizeMs);
         ALOGV("%p kUseFastCapture = Static, format = 0x%x, (%lld * 1000) / %u vs %u, "
                 "initFastCapture = %d, mIsMsdDevice = %d", this, mFormat, (long long)mFrameCount,
                 mSampleRate, kMinNormalCaptureBufferSizeMs, initFastCapture, mIsMsdDevice);
