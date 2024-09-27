d2828c050c01d839406ad911db57e7243b578142
Xuewen Liu <quic_xuewliu@quicinc.com>
2024-09-27 14:54:16 +0800

    framework: add aaudio performance mode for APTX HD audio
    Add missed code of aaudio performance mode for APTX HD audio case
    
    Change-Id: If5d0764798285c130c1802dd8e7a31c39e1f784a
    Signed-off-by: Xuewen Liu <quic_xuewliu@quicinc.com>
    (cherry picked from commit 1ff1930629042f2760f8286e348075eb02388151)

--- media/libaaudio/src/core/AudioStreamBuilder.cpp
+++ media/libaaudio/src/core/AudioStreamBuilder.cpp
@@ -255,6 +255,7 @@ aaudio_result_t AudioStreamBuilder::validate() const {
         case AAUDIO_PERFORMANCE_MODE_NONE:
         case AAUDIO_PERFORMANCE_MODE_POWER_SAVING:
         case AAUDIO_PERFORMANCE_MODE_LOW_LATENCY:
+        case AAUDIO_PERFORMANCE_MODE_HD_APTX:
             break;
         default:
             ALOGE("illegal performanceMode = %d", mPerformanceMode);
