3d9dcd3e5bea8e798755de5ef35eb01a9c4aa948
Zihao Lin <quic_zihalin@quicinc.com>
2024-11-07 07:36:50 +0530

    audiopolicy: select proper stream type for r_submix
    Change-Id: Ifcac2f82c782b5490c122c943bd9c457da567d3e
    Signed-off-by: Zihao Lin <quic_zihalin@quicinc.com>
    (cherry picked from commit 4814be79501a237a2278cccbef608af4b7a0fa0c)

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -1397,19 +1397,19 @@ status_t AudioPolicyManager::getOutputForAttrInt(
                     mDirectOutput = policyDesc;
                 }
             }
-            if (policyDesc != nullptr) {
-                policyDesc->mPolicyMix = primaryMix;
-                *output = policyDesc->mIoHandle;
-                *selectedDeviceId = deviceDesc != 0 ? deviceDesc->getId() : AUDIO_PORT_HANDLE_NONE;
+        }
+        if (policyDesc != nullptr) {
+            policyDesc->mPolicyMix = primaryMix;
+            *output = policyDesc->mIoHandle;
+            *selectedDeviceId = deviceDesc != 0 ? deviceDesc->getId() : AUDIO_PORT_HANDLE_NONE;
 
-                ALOGD("getOutputForAttr() returns output %d selectedDeviceId %d", *output, *selectedDeviceId);
-                if (resultAttr->usage == AUDIO_USAGE_VIRTUAL_SOURCE) {
-                    *outputType = API_OUT_MIX_PLAYBACK;
-                } else {
-                    *outputType = API_OUTPUT_LEGACY;
-                }
-                return NO_ERROR;
+            ALOGD("getOutputForAttr() returns output %d selectedDeviceId %d", *output, *selectedDeviceId);
+            if (resultAttr->usage == AUDIO_USAGE_VIRTUAL_SOURCE) {
+                *outputType = API_OUT_MIX_PLAYBACK;
+            } else {
+                *outputType = API_OUTPUT_LEGACY;
             }
+            return NO_ERROR;
         }
     }
     // Virtual sources must always be dynamicaly or explicitly routed
