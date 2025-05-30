857ae191510912e649aad76859dee65e2fcbffa6
Jayabal Arasappan <quic_jarasapp@quicinc.com>
2025-05-30 12:32:56 +0530

    Disable instantiateVRAudioServer for IOT low RAM configuration
    Change-Id: I7986da909e95e8b41e37ce832b5f6833e92ec7f7

--- media/audioserver/main_audioserver.cpp
+++ media/audioserver/main_audioserver.cpp
@@ -82,7 +82,6 @@ int main(int argc __unused, char **argv)
 #else
     bool doLog = (bool) property_get_bool("ro.test_harness", 0);
 #endif
-    bool enableMemOptimize = (bool) property_get_bool("ro.product.memopt.enable",1);
 
     pid_t childPid;
     // FIXME The advantage of making the process containing media.log service the parent process of
@@ -189,10 +188,9 @@ int main(int argc __unused, char **argv)
 
         // Add AudioFlinger and AudioPolicy to ServiceManager.
         sp<IServiceManager> sm = defaultServiceManager();
-        if (!enableMemOptimize) {
+#ifndef TARGET_QCOM_IOT_LOW_RAM
             instantiateVRAudioServer();
-            ALOGD("%s: VRAudioServer instantiate done %p", __func__, sm.get());
-        }
+#endif
         sm->addService(String16(IAudioFlinger::DEFAULT_SERVICE_NAME), afAdapter,
                 false /* allowIsolated */, IServiceManager::DUMP_FLAG_PRIORITY_DEFAULT);
         sm->addService(String16(AudioPolicyService::getServiceName()), aps,
