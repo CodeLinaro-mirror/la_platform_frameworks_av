ef7667c82c8632cbf465b06ca0a98f5d7886a21d
Venu Gopal Atchyutanna <vatchyut@qti.qualcomm.com>
2025-09-16 12:21:03 +0530

    audioserver: register the IHalAdapterVendorExtension(IHAVE)
    As IHAVE is expected to be system_ext service, register
    the HAVE AIDL service as part of audioserver context.
    
    Note, Audio HAL and IHAVE service are semantically linked.
    Thus, this IHAVE implementation is for AIDL HAL.
    
    Change-Id: I836b5fd67e5f0e99f0d64d5f95d98a9efb53dd56
    Signed-off-by: Venu Gopal Atchyutanna <vatchyut@qti.qualcomm.com>
    (cherry picked from commit 0b15d3c7a92f3ef7089c4e1f4df0c5fe351fda6d)
    (cherry picked from commit ebcfeb42ba5cf96c2da8d62569139584834f2fa3)

--- media/audioserver/main_audioserver.cpp
+++ media/audioserver/main_audioserver.cpp
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "audioserver_main"
@@ -23,7 +28,7 @@
 #include <sys/prctl.h>
 #include <sys/wait.h>
 #include <cutils/properties.h>
-
+#include <dlfcn.h>
 #include <android/media/audio/common/AudioMMapPolicy.h>
 #include <android/media/audio/common/AudioMMapPolicyInfo.h>
 #include <android/media/audio/common/AudioMMapPolicyType.h>
@@ -47,6 +52,24 @@ using android::media::audio::common::AudioMMapPolicy;
 using android::media::audio::common::AudioMMapPolicyInfo;
 using android::media::audio::common::AudioMMapPolicyType;
 
+void registerIHalAdapterVendorExtension() {
+    constexpr char kLibPath[] = "libaudiohalvendorextn.so";
+    void *libHandle = dlopen(kLibPath, RTLD_NOW | RTLD_NODELETE);
+    if (libHandle == nullptr) {
+        ALOGE("Failed to load library: %s (%s)", kLibPath, dlerror());
+        return;
+    }
+
+    auto registerInterface =
+        reinterpret_cast<void (*)()>(dlsym(libHandle, "registerInterface"));
+    if (registerInterface == nullptr) {
+        ALOGE("Failed to find symbol(registerInterface): error (%s)",
+              dlerror());
+        return;
+    }
+    registerInterface();
+}
+
 int main(int argc __unused, char **argv __unused)
 {
     SLOGI("%s: starting", __func__);
@@ -64,6 +87,9 @@ int main(int argc __unused, char **argv __unused)
     // Ensure threads for possible callbacks.  Note that get_audio_flinger() does
     // this automatically when called from AudioPolicy, but we do this anyways here.
     ProcessState::self()->startThreadPool();
+
+    // Making sure this service is registered before the flinger.
+    registerIHalAdapterVendorExtension();
 
     // Instantiating AudioFlinger (making it public, e.g. through ::initialize())
     // and then instantiating AudioPolicy (and making it public)
