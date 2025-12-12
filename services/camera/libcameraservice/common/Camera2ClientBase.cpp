msft-mirror-aosp/platform.frameworks.av

Camera2 multi-client: Store clients based on PID.
In camera2 multi-client, currently clients are stored in the AidlCamera3SharedDevice
based on uid. That should be changed to PID as the same uid can have multiple processes
which can access the same camera.

Flag: com.android.internal.camera.flags.camera_multi_client
Bug:389159564
Test: run CTS test SharedCameraTest#testCameraDeviceSharingSupported and
verify in the logcat that HAL level camera disconnect only happens when
last client of the camera disconnects.

Change-Id: I6300426caf5bc0a1033e983d0ff3c552e45b625b

services/camera/libcameraservice/common/Camera2ClientBase.cpp
@@ -304,7 +304,7 @@ template <typename TClientBase>
 void Camera2ClientBase<TClientBase>::detachDevice() {
     if (mDevice == 0) return;
     if (flags::camera_multi_client() && TClientBase::mSharedMode) {
-        mDevice->disconnectClient(TClientBase::getClientUid());
+        mDevice->disconnectClient(TClientBase::getClientCallingPid());
     } else {
         mDevice->disconnect();
     }

CodeLinaro-mirror/la_platform_frameworks_av

services/camera/libcameraservice/common/Camera2ClientBase.cpp
@@ -315,7 +315,7 @@ template <typename TClientBase>
 void Camera2ClientBase<TClientBase>::detachDevice() {
     if (mDevice == 0) return;
     if (flags::camera_multi_client() && TClientBase::mSharedMode) {
-        mDevice->disconnectClient(TClientBase::getClientUid());
+        mDevice->disconnectClient(TClientBase::getClientPid());
     } else {
         mDevice->disconnect();
     }